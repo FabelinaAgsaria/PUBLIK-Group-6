ea86bb5235ec25f8897fad0167ae1697374ab746
FabelinaAgsaria <fabelinaagsaria2004@gmail.com>
2023-12-28 15:30:34 +0700

    Fabelina_Agsaria_E1E122096

--- aplikasi/konek.php
+++ aplikasi/konek.php
@@ -1,13 +1,18 @@
 <?php
-// E1E122004 ANNISA NURFADILAH
+
 class Database
 {
+   
+    
     // Properti dengan visibility diawali sebelum properti untuk menambahkan kejelasan dalam pembacaan kode.
     private $host = "localhost", $username = "root", $password = "", $db = "db_publik", $conn;
+    //Komen UAS_2 : Konsep Private di atas berarti hanya bisa di akses di dalam class parent saja di luar kelas tidak bisa di akses
 
     // Konstruktor, merupakan magic method pada PHP, digunakan untuk inisialisasi objek saat objek dibuat.
     public function __construct()
     {
+    //Komen UAS_2 : Konsep Public bisa di akses di luar kelas
+       
         // Menghubungkan ke database menggunakan properti yang telah ditentukan sebelumnya.
         $this->conn = mysqli_connect($this->host, $this->username, $this->password, $this->db);
     }
